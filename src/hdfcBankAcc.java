6b1120767fca4bfa30b5cfc7bd8f4e83a489c837
Neelam Dubey <ashidubey8208@gmail.com>
2023-05-12 21:33:20 +0530

    implement money

--- src/hdfcBankAcc.java
+++ src/hdfcBankAcc.java
@@ -1,2 +1,20 @@
 public class hdfcBankAcc implements bankacc{
+    private int balance;
+    public hdfcBankAcc(int balance)
+    {
+        this.balance=balance;
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+    @Override
+    public boolean withdrawMoney(int Money) {
+        if (this.balance >= Money) {
+            this.balance = this.balance - Money;
+            return true;
+        }
+        return false;
+    }
 }
--- src/sbiBankAcc.java
+++ src/sbiBankAcc.java
@@ -1,2 +1,20 @@
 public class sbiBankAcc implements bankacc{
+    private int balance;
+    public sbiBankAcc(int balance)
+    {
+        this.balance=balance;
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+    @Override
+    public boolean withdrawMoney(int Money) {
+        if(this.balance >=Money)
+        {
+            this.balance=this.balance-money;
+            return true;
+        }
+        return false;
+    }
 }
